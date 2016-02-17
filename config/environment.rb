6598b4e14ed7b790e94509e0ecdbb8cce0377e97
dylandy.chang <dylandy.chang@gmail.com>
2016-02-17 15:56:32 +0800

    added dir changing method in scarlet binary file to ensure every command would run in root file of our application

--- config/environment.rb
+++ config/environment.rb
@@ -4,17 +4,9 @@ Bundler.require(:default , env )
 
 #check if database.yml exist or not then create the db connection 
 if `ls ./config/`.split("\n").include? "database.yml" 
-  #check if routes.yml exist or not  
-  if `ls ./config/`.split("\n").include? "routes.yml" 
-    dbconfig = YAML.load(open("./config/database.yml"))
-    ActiveRecord::Base.logger = Logger.new(STDOUT)
-    ActiveRecord::Base.establish_connection dbconfig[env]
-  else
-    system("cp ./config/routes.yml.example ./config/routes.yml")
-    dbconfig = YAML.load(open("./config/database.yml"))
-    ActiveRecord::Base.logger = Logger.new(STDOUT)
-    ActiveRecord::Base.establish_connection dbconfig[env]
-  end
+  dbconfig = YAML.load(open("./config/database.yml"))
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
+  ActiveRecord::Base.establish_connection dbconfig[env]
 else
   system("cp ./config/database.yml.example ./config/database.yml")
 end
